b964878c4ab1d2730d4f30d79509bd4bcd596763
seono9y <144989211+seono9y@users.noreply.github.com>
2023-10-08 18:09:41 +0900

    05

--- exercise_05/exercise_05/main.c
+++ exercise_05/exercise_05/main.c
@@ -8,17 +8,26 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    int num;
-    int sum = 0;
+    int num1, num2;
+    char operator;
+    int result;
 
-    printf("정수를 입력하세요: ");
-    scanf("%d", &num);
+    printf("두 개의 정수와 연산자를 입력하세요 : ");
+    scanf("%d %c %d", &num1, &operator, &num2);
 
-    for (int i = 1; i <= num; i++) {
-        sum += i;
+    if (operator == '+') {
+        result = num1 + num2;
+    } else if (operator == '-') {
+        result = num1 - num2;
+    } else if (operator == '*') {
+        result = num1 * num2;
+    } else if (operator == '/') {
+        if (num2 != 0) {
+            result = num1 / num2;
+        }
     }
 
-    printf("1부터 %d까지의 합은 %d입니다.\n", num, sum);
+    printf("%d %c %d = %d\n", num1, operator, num2, result);
 
     return 0;
 }
