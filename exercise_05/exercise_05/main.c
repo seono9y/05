892c86d219fbb5fe3793a01636e1a79090319662
seono9y <144989211+seono9y@users.noreply.github.com>
2023-10-08 18:01:14 +0900

    04

--- exercise_05/exercise_05/main.c
+++ exercise_05/exercise_05/main.c
@@ -8,18 +8,17 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    char c;
-    int numCount = 0;
+    int num;
+    int sum = 0;
 
-    printf("문자열을 입력하세요 : ");
+    printf("정수를 입력하세요: ");
+    scanf("%d", &num);
 
-    while ((c = getchar()) != '\n') {
-        if (c >= '0' && c <= '9') {
-            numCount++;
-        }
+    for (int i = 1; i <= num; i++) {
+        sum += i;
     }
 
-    printf("입력된 문자열 내에 숫자의 개수는 %d개 입니다.\n", numCount);
+    printf("1부터 %d까지의 합은 %d입니다.\n", num, sum);
 
     return 0;
 }
